2d37f723ee8cbdb600e263a115f8566afd046236
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-07-31 21:49:40 +0200

    Add project files.

---
+++ SklepNet_MVC/Models/Sklep/Zamowienie.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SklepNet_MVC.Models.Sklep
+{
+    public class Zamowienie
+    {
+        [Key]
+        public int IdZamowienie { get; set; }
+
+        public int IdHandlowiec { get; set; }
+        public virtual Handlowiec Handlowiec { get; set; }
+        [Required(ErrorMessage = "Imię jest wymagane.")]
+        public string Imie { get; set; }
+        [Required(ErrorMessage = "Nazwisko jest wymagane.")]
+        public string Nazwisko { get; set; }
+        [Required(ErrorMessage = "Adres jest wymagany.")]
+        public string Adres { get; set; }
+        [Required(ErrorMessage = "Telefon jest wymagany.")]
+        public string Telefon { get; set; }
+        [Required(ErrorMessage = "Email jest wymagany.")]
+        public string Email { get; set; }
+        public DateTime DataZamowienia { get; set; }
+        public virtual ICollection<ZamowieniePozycja> ZamowieniePozycja
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ SklepNet_MVC/Models/Sklep/ZamowieniePozycja.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SklepNet_MVC.Models.Sklep
+{
+    public class ZamowieniePozycja
+    {
+        [Key]
+        public int IdZamowieniePozycja { get; set; }
+        public int IdTowar { get; set; }
+        public virtual Towar Towar { get; set; }
+        public int Ilosc { get; set; }
+        public decimal Cena { get; set; }
+
+        public decimal Wartosc { get; set; }
+        public int IdZamowienie { get; set; }
+        public Zamowienie Zamowienie { get; set; }
+    }
+}
